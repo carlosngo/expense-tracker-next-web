da3a83a13bde572d63d9cefe5288a3be3c844b99
Carlos <carlos_ngo@dlsu.edu.ph>
2022-04-17 22:56:39 +0800

    setup home page

---
+++ components/TransactionTable.tsx
@@ -0,0 +1,10 @@
+import {Transaction} from "../interfaces/transaction";
+
+interface Props {
+    data: Transaction[],
+    isLoading: boolean
+}
+
+const TransactionTable = (props: Props) => {
+
+}
